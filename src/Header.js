5e9a945666f5ffa7d0b279025c912bf145a4623d
David Cheung <david.cheung@continuumeconomics.com>
2020-04-26 19:30:51 -0400

    ENH: branding davidcheung styling

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,16 @@ function Header() {
     return (
         <header className="masthead mb-auto">
             <div className="inner">
-                <h3 className="masthead-brand">davidcheung</h3>
+                <span className="masthead-brand">
+                    <span>
+                        <img className="brand" src="/favicon.ico" />
+                        &nbsp;
+                    </span>
+                    <span>
+                        david<strong>cheung</strong>
+                    </span>
+                </span>
+
                 <nav className="nav nav-masthead justify-content-center">
                     <a className="nav-link active" href="#">
                         Home

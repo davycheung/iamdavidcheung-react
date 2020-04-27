8694fe711b278dbc2817841a8ae25700988814d6
David Cheung <david.cheung@continuumeconomics.com>
2020-04-27 17:50:05 -0400

    ENH: about component

---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function About() {
+    return (
+        <main role="main" className="inner cover text-left">
+            <h1 className="cover-heading">About</h1>
+            <p>
+                Engineering Lead at{" "}
+                <a href="http://www.climbcredit.com">Climb Credit</a>, helping
+                students build careers.
+            </p>
+            <p>
+                Previously Head of Engineering at{" "}
+                <a href="http://www.continuumeconomics.com">
+                    Continuum Economics
+                </a>
+                , building a research distribution platform delivering economic
+                research to trading desks and key decision makers throughout the
+                world.
+            </p>
+            <p>
+                B.S. Computer Science & M.S. Applied Mathematics at Stony Brook
+                University. When not working you can find me at a variety of
+                meetups, hiking, reading, and otherwise enjoying life.
+            </p>
+        </main>
+    );
+}
+
+export default About;

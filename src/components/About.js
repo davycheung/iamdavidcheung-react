5dcacd8eb13e959902d96a090bc6e80ea082cc07
David Cheung <david.cheung@continuumeconomics.com>
2020-04-28 20:58:07 -0400

    FIX: about page `text-left`

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
     return (
-        <div>
+        <div className="text-left">
             <h1 className="cover-heading">About</h1>
             <p>
                 Engineering Lead at{" "}

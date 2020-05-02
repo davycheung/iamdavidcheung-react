4470e6a1c52dfca830507ef8db07cedd2029b79b
David Cheung <david.cheung@continuumeconomics.com>
2020-05-01 22:52:03 -0400

    CLN jquery

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 // import * as serviceWorker from "./serviceWorker";
+import "jquery/dist/jquery.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 render(
